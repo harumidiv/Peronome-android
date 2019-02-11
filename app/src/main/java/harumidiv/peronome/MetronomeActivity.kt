de6a884dc293bc22cd3e40804f56135049ec00b8
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-02-11 23:48:30 +0900

    feat: Create peronome project

---
+++ app/src/main/java/harumidiv/peronome/MetronomeActivity.kt
@@ -0,0 +1,12 @@
+package harumidiv.peronome
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MetronomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
