c20de87499d17e0d2bd5659fd222a432664795a1
佐川晴海 <sagawaharumi@sagawaharuminoMacBook-puro.local>
2019-02-24 22:58:13 +0900

    fix: Ficed layout id name

--- app/src/main/java/harumidiv/peronome/MetronomeActivity.kt
+++ app/src/main/java/harumidiv/peronome/MetronomeActivity.kt
@@ -2,8 +2,19 @@ package harumidiv.peronome
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class MetronomeActivity : AppCompatActivity(), MetronomePresenterOutput {
+
+    private var pendlumImage:ImageView? =null
+    private var tempoLabel: TextView? = null
+    private var addTempoButton: ImageButton? = null
+    private var subTempoButton: ImageButton? = null
+    private var startStopButton: ImageButton? = null
+
+    private var presenter: MetronomePresenter? = null
 
     /**
      * lifecycle
@@ -11,10 +22,16 @@ class MetronomeActivity : AppCompatActivity(), MetronomePresenterOutput {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pendlumImage = findViewById(R.id.pendlumImage)
+        tempoLabel = findViewById(R.id.tempoLabel)
+        addTempoButton = findViewById(R.id.addTempoButton)
+        subTempoButton = findViewById(R.id.subTempoButton)
+        startStopButton = findViewById(R.id.startStopButton)
+        presenter = MetronomePresenter(this, 120.0)
     }
 
     /**
-     *　presenter output
+     *　presenter output method
      */
     override fun showLabel(tempo: String) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
